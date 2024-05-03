371e177c89f378e067bb72eb15d1758cad55b657
jsesbasti <jsebasti@sudent.42barcelona.com>
2024-05-03 08:56:53 +0200

    all

--- inc/network/Socket.hpp
+++ inc/network/Socket.hpp
@@ -6,7 +6,7 @@
 /*   By: jsebasti <jsebasti@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/26 13:27:44 by jsebasti          #+#    #+#             */
-/*   Updated: 2024/04/26 13:31:15 by jsebasti         ###   ########.fr       */
+/*   Updated: 2024/05/03 08:49:03 by jsebasti         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,7 +18,7 @@
 class Socket
 {
 private:
-    int sock_fd;
+    socket_f sock_fd;
 public:
     Socket( void );
     ~Socket( void );
--- src/network/Socket.cpp
+++ src/network/Socket.cpp
@@ -6,12 +6,22 @@
 /*   By: jsebasti <jsebasti@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/26 13:31:30 by jsebasti          #+#    #+#             */
-/*   Updated: 2024/04/26 13:59:06 by jsebasti         ###   ########.fr       */
+/*   Updated: 2024/05/03 08:52:20 by jsebasti         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <Socket.hpp>
 
 Socket::Socket( void ) {
-    this->sock_fd = socket(PF_INET, SOCK_STREAM, );
+    return ;
+}
+
+Socket::~Socket( void ) {
+    return ;
+}
+
+socket_t    Socket::createSocket( int domain, int type, int protocol ) {
+    this->sock_fd = socket(domain, type, protocol);
+    if (this->sock_fd < 0)
+        throw std::runtime_error("Error setting the socket for ")
 }
